9be3f776ab2d0f6371103c750c97b0582959aefe
Rodolfo José <rodolfo.santos@exemplo.com>
2019-05-14 17:44:18 -0300

    aula modificador internal

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
 
 namespace ByteBank.SistemaAgencia
 {
@@ -10,7 +12,16 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
+            ContaCorrente conta = new ContaCorrente(123, 12345);
+
+
+            Console.WriteLine(conta.Numero);
+
+            GerenteDeConta gerente = new GerenteDeConta("2432345254");
+            gerente.Autenticar("123");
             
+
+            Console.ReadLine();
         }
     }
 }
